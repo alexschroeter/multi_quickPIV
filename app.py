d77e0bd5fc3753382a305b72de4c6f20520cd656
Alexander Schröter <me@alexanderschroeter.net>
2024-06-05 12:29:29 +0000

    Added an example with reference data

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from arkitekt import register
 import time
 from mikro.api.schema import (
     RepresentationFragment,
-    from_xarray
+    from_xarray,
 )
 
 from juliacall import Main as jl
@@ -12,8 +12,8 @@ import matplotlib.pyplot as plt
 
 @dataclass
 class PIVResult:
-    u : np.ndarray
-    v : np.ndarray
+    VF : np.ndarray
+    SN : np.ndarray
 
 @register
 def multi_quickPIV(image1 : RepresentationFragment,
@@ -22,7 +22,7 @@ def multi_quickPIV(image1 : RepresentationFragment,
                 #    interSize = (32,32,32),
                 #    searchMargin = (10,10,10),
                 #    threshold = (1000),
-                   ):
+                   ) -> PIVResult:
     """
     This function performs a multithreaded Particle Image Velocimitry (PIV) on two images.
     The results are returned in form of a displacment vector for each interrogation window.
@@ -53,8 +53,7 @@ def multi_quickPIV(image1 : RepresentationFragment,
     tuple : (np.ndarray, np.ndarray)
         tuple whose first element is the PIV vector field, and the second element is the signal-to-noise ratio
     """
-    
-
+    print("starting")
     # load the data of image1 and image2 and convert it to numpy array
     img1 = image1.data.sel(c=0, t=0).data.compute()
     img2 = image2.data.sel(c=0, t=0).data.compute()
@@ -63,45 +62,21 @@ def multi_quickPIV(image1 : RepresentationFragment,
     # img2 = np.random.rand(100, 100)    
 
     # run PIV
+    print(jl.seval("Threads.nthreads()"))
     jl.seval("using multi_quickPIV")
     #pivparams = jl.multi_quickPIV.setPIVParameters( interSize=jl.Array(interSize), searchMargin=jl.Array(searchMargin), step=jl.Array(step), threshold=jl.Array(threshold) )
     # VF, SN = jl.multi_quickPIV.PIV( jl.Array(img1), jl.Array(img2), pivparams )
     VF, SN = jl.multi_quickPIV.PIV( jl.Array(img1), jl.Array(img2))
 
-    # to vizualize the results we need to manipulte the data
-    np_u = np.array(VF)[0]
-    np_v = np.array(VF)[1]
-    vfsize = np_u.shape
-
-    # Creating the x and y coordinates for the quiver plot
-    # step   = np.array(jl.multi_quickPIV._step( pivparams )[1:2])
-    step   = (32, 32)
-    # isize  = np.array(jl.multi_quickPIV._isize( pivparams )[1:2])
-    isize  = (32, 32)
-    xgrid = np.array([ (x)*step[1] + isize[1]//2 for y in range(vfsize[0]) for x in range(vfsize[1]) ]).reshape(vfsize)
-    ygrid = np.array([ (y)*step[0] + isize[0]//2 for y in range(vfsize[0]) for x in range(vfsize[1]) ]).reshape(vfsize)
-
-    # Plotting
-    plt.figure(figsize=(15, 5))
-
-    plt.subplot(1, 3, 1)
-    plt.title("img1")
-    plt.imshow(img1, cmap="Reds")
-
-    plt.subplot(1, 3, 2)
-    plt.title("img2")
-    plt.imshow(img2, cmap="Blues")
-
-    plt.subplot(1, 3, 3)
-    plt.title("PIV")
-    plt.imshow(img1, cmap="Reds")
-    plt.imshow(img2, alpha=0.5, cmap="Blues")
-    plt.quiver(xgrid, ygrid, np_v, np_u * -1, color="red", scale=50)
-    plt.savefig("plot.png")
-
+    print("Computed PIV successfully")
+    print("VF: ", np.array(VF))
+    print("SN: ", np.array(SN))
+    return PIVResult(VF=np.array(VF), SN=np.array(SN))
 
 @register
-def plotPIV(PIVResult : PIVResult,
+def plotPIV(image1 : RepresentationFragment,
+            image2 : RepresentationFragment,
+            PIVResult : PIVResult,
             ) -> RepresentationFragment:
     """
     This function plots the PIV result
@@ -132,10 +107,14 @@ def plotPIV(PIVResult : PIVResult,
     vfsize = U.shape
 
     # Creating the x and y coordinates for the quiver plot
-    step   = np.array(jl.multi_quickPIV._step( pivparams )[1:2])
-    isize  = np.array(jl.multi_quickPIV._isize( pivparams )[1:2])
+    step   = (32, 32)
+    isize  = (32, 32)
     xgrid = np.array([ (x)*step[1] + isize[1]//2 for y in range(vfsize[0]) for x in range(vfsize[1]) ]).reshape(vfsize)
     ygrid = np.array([ (y)*step[0] + isize[0]//2 for y in range(vfsize[0]) for x in range(vfsize[1]) ]).reshape(vfsize)
+
+    # load the data of image1 and image2 and convert it to numpy array
+    img1 = image1.data.sel(c=0, t=0).data.compute()
+    img2 = image2.data.sel(c=0, t=0).data.compute()
 
     # Plotting
     plt.figure(figsize=(15, 5))
@@ -152,5 +131,98 @@ def plotPIV(PIVResult : PIVResult,
     plt.title("PIV")
     plt.imshow(img1, cmap="Reds")
     plt.imshow(img2, alpha=0.5, cmap="Blues")
-    plt.quiver(xgrid, ygrid, np_v, np_u * -1, color="red", scale=50)
+    plt.quiver(xgrid, ygrid, V, U * -1, color="red", scale=50)
+    plt.savefig("piv.png")
+
+    with open("piv.png", "rb") as f:
+        plot = ImageFile(data=f.read())
+
     return from_xarray(plot)
+
+@register
+def example_PIV() -> None:
+    """
+    This function is an example of how to use the PIV functions
+    """
+    import urllib.request
+    gif_ = urllib.request.urlretrieve("https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExY2FlOXJ6dHJjem5xNHZmaTVkMDdmMHZkanZvbGx1bDZ4b2d6M25sYSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/26xBuTV58WNJRlhLi/giphy.gif", "file.gif")
+    from PIL import Image
+    gif = Image.open('file.gif')
+
+    frames = []
+    # Loop through each frame in the GIF
+    try:
+        while True:
+            # Convert the frame to an array and append to the list
+            frame_array = np.array(gif.convert('L')) # Convert to grayscale
+            frames.append(frame_array)
+
+            # Move to the next frame
+            gif.seek(gif.tell() + 1)
+    except EOFError:
+        # End of the sequence
+        pass
+
+    frames = np.array(frames)
+    img1 = frames[6, :, :]
+    img2 = frames[7, :, :]
+
+    jl.seval("using multi_quickPIV")
+    VF, SN = jl.multi_quickPIV.PIV( jl.Array(img1), jl.Array(img2))
+
+    """
+    We create a plot of the two Input Images and the resulting PIV vectors between them
+    """
+    VF = np.array(VF)
+    SN = np.array(SN)
+
+    U = VF[ 0, :, : ]
+    V = VF[ 1, :, : ]
+    vfsize = U.shape
+
+    # Creating the x and y coordinates for the quiver plot
+    step   = (32, 32)
+    isize  = (32, 32)
+    xgrid = np.array([ (x)*step[1] + isize[1]//2 for y in range(vfsize[0]) for x in range(vfsize[1]) ]).reshape(vfsize)
+    ygrid = np.array([ (y)*step[0] + isize[0]//2 for y in range(vfsize[0]) for x in range(vfsize[1]) ]).reshape(vfsize)
+
+    plt.figure(figsize=(15, 5))
+
+    plt.subplot(1, 3, 1)
+    plt.title("Input Image 1 (Frame 6)")
+    plt.imshow(img1, cmap="Reds")
+
+    plt.subplot(1, 3, 2)
+    plt.title("Input Image 2 (Frame 7)")
+    plt.imshow(img2, cmap="Blues")
+
+    plt.subplot(1, 3, 3)
+    plt.title("PIV between Frame 6 and 7")
+    plt.imshow(img1, cmap="Reds")
+    plt.imshow(img2, alpha=0.5, cmap="Blues")
+    plt.quiver(xgrid, ygrid, V, U * -1, color="red", scale=50)
+    plt.savefig("example_arkitekt_piv.png")
+
+    """
+    We compare the results of the original PIV and the Arkitekt implementation
+    """
+    ground_truth = np.load("example_groundtruth.npz")
+    diff = np.abs(VF, ground_truth)
+    plt.clf()
+    plt.figure(figsize=(15, 5))
+
+    plt.subplot(1, 2, 1)
+    plt.imshow(diff[0], cmap='hot', interpolation='nearest')
+    plt.title("U")
+    plt.xlabel('Column Index')
+    plt.ylabel('Row Index')
+    plt.colorbar(label='Absolute Difference')
+
+    plt.subplot(1, 2, 2)
+    plt.imshow(diff[1], cmap='hot', interpolation='nearest')
+    plt.title("V")
+    plt.xlabel('Column Index')
+    plt.ylabel('Row Index')
+    plt.colorbar(label='Absolute Difference')
+
+    plt.savefig("diff.png")
